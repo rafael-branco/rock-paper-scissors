e8cd7ca7eb1f97dbab520b2459a24d7fd897f9b7
rafael-branco <rafael.rbranco@hotmail.com>
2024-12-06 14:08:21 -0300

    Add checkPlay and updateScore functions

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 
+
+function updateScore(score){
+    let scoreElement = document.getElementById('score');
+    scoreElement.innerHTML = score;
+}
+
+function checkPlay(player, bot){
+    if(player == bot){
+        return 0;
+    }else if(player == "paper" && bot == "rock"){
+        return 1;
+    }else if(player == "paper" && bot == "scissors"){
+        return -1;
+    }else if(player == "rock" && bot == "paper"){
+        return -1;
+    }else if(player == "rock" && bot == "scissors"){
+        return 1;
+    }else if(player == "scissors" && bot == "rock"){
+        return -1;
+    }else if(player == "scissors" && bot == "paper"){
+        return 1;
+    }else{
+        return null;
+    }
+}
+
+function playGame(){
+    let player;
+    let bot;
+
+
+}
